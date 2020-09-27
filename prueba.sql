963b3fd49273d8dd6c6ca197e164aefe4f59d8b5
Romina Pineda <romi.macka@gmail.com>
2020-09-26 23:29:04 -0300

    Insert Values

--- prueba.sql
+++ prueba.sql
@@ -10,7 +10,7 @@ CREATE TABLE categoria(
     PRIMARY KEY(id)
 );
 CREATE TABLE producto(
-    id INT NOT NULL UNIQUE,
+    id SERIAL,
     nombre VARCHAR(30) NOT NULL,
     descripcion TEXT,
     valor_unitario INT NOT NULL,
@@ -22,7 +22,9 @@ CREATE TABLE factura(
     id SERIAL,
     numero_factura INT NOT NULL,
     fecha_creacion DATE NOT NULL,
-    PRIMARY KEY (id)
+    id_cliente INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id) 
 );
 CREATE TABLE producto_factura(
     id_producto INT NOT NULL,
@@ -36,7 +38,47 @@ CREATE TABLE cliente(
     nombre VARCHAR(30) NOT NULL,
     rut VARCHAR(10) NOT NULL UNIQUE,
     direccion VARCHAR(100) NOT NULL,
-    id_factura INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY(id_factura) REFERENCES factura(id) 
+    PRIMARY KEY (rut),
 );
+-- Ingresa registros
+INSERT INTO categoria (nombre_cat, descripcion) 
+	VALUES ('videojuegos', 'Lo último en videojuegos.'),
+    ('musica', 'Tecnología musical de vanguardia.'),
+	('deporte', 'Aumenta tu rendimiento físico.');
+INSERT INTO producto (nombre, descripcion, valor_unitario, id_categoria) 
+	VALUES ('God of War', 'God of War 4 PS4', 20990, 1),
+    ('Joystick', 'PS Remote', 40990, 1),
+	('Guitarra', 'Acústica', 25000, 2),
+    ('Flauta', 'Dulce', 7000, 2),
+    ('Uñeta', 'Extra duración', 3000, 2),
+    ('Pelota', 'Fútbol', 24900, 3),
+    ('Caminadora', 'Plegable', 125000, 3),
+    ('Guantes', 'Boxeo, Kickboxing', 12000, 3);
+INSERT INTO factura (numero_factura, fecha_creacion, id_cliente) 
+	VALUES (1, '2020/09/15', 1),
+    (2, '2020/09/16', 1),
+	(1, '2020/09/17', 2),
+    (2, '2020/09/18', 2),
+    (3, '2020/09/17', 2),
+    (1, '2020/09/18', 3),
+    (1, '2020/09/19', 4),
+    (2, '2020/09/20', 4),
+    (3, '2020/09/21', 4),
+    (4, '2020/09/22', 4);
+INSERT INTO producto_factura (id_producto, id_factura, cantidad) 
+	VALUES (1, 1, 2),
+    (2, 2, 3),
+	(3, 3, 3),
+    (4, 4, 2),
+    (5, 5, 3),
+    (6, 6, 1),
+    (7, 7, 2),
+    (8, 8, 3),
+    (9, 9, 4),
+    (10, 10, 1);
+INSERT INTO cliente (nombre, rut, direccion) 
+	VALUES ('Claudia', '17118258-1', 'Mujica 55'),
+    ('María', '18170684-8', 'Salvador 15'),
+	('Fresia', '17345987-0', 'Errázuriz 2456'),
+    ('Soledad', '21332543-8', 'Alameda 000'),
+    ('Carla', '19324543-2', 'Playa Ancha 33');
